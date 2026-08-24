7d24da78a4de712856b67efc898c4cc3141c7ecb
Ashok Neupane <ashokabrother@gmail.com>
2026-08-24 06:32:00 +0545

    Every details of javaScript

---
+++ index.js
@@ -0,0 +1,17 @@
+//console.log(`Hello, World!`);
+//console.log(`I like Pizza`);
+//window.alert(`This is an alert!`);
+//window.alert(`I like Pizza`);
+document.getElementById(`myH1`).textContent = `Hello`;
+document.getElementById(`myP`).textContent = `I like Pizza`;
+
+
+
+/*
+This is a comment for everyone,
+
+
+
+
+
+*/
