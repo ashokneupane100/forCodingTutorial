4e66a7a9a09394e0d78a75affbbb61c2b8aff6bb
Ashok Neupane <ashokabrother@gmail.com>
2026-08-24 07:17:44 +0545

    knowing the variables.

--- index.js
+++ index.js
@@ -1,17 +1,76 @@
-//console.log(`Hello, World!`);
-//console.log(`I like Pizza`);
-//window.alert(`This is an alert!`);
-//window.alert(`I like Pizza`);
-document.getElementById(`myH1`).textContent = `Hello`;
-document.getElementById(`myP`).textContent = `I like Pizza`;
+// //console.log(`Hello, World!`);
+// //console.log(`I like Pizza`);
+// //window.alert(`This is an alert!`);
+// //window.alert(`I like Pizza`);
+// document.getElementById(`myH1`).textContent = `Hello`;
+// document.getElementById(`myP`).textContent = `I like Pizza`;
 
 
 
-/*
-This is a comment for everyone,
+// /*
+// This is a comment for everyone,
+// */
+
+// let x;
+// x = 123;
+// console.log(x);
 
 
+// // let age=25;
+// // console.log(age);
+
+// // let price="expensive";
+// // console.log(price);
+
+// // let gpa=2.5;
+// // console.log(`You are ${age} years old and your GPA is ${gpa}.`);
+
+// // console.log(`The price of the item is $${price}.`);
+// // console.log(`My gpa is ${gpa} but I am talented
+// //     in practical skills of programming.`)
+
+// // console.log(typeof age);
+// // console.log(typeof price);
+// // console.log(typeof gpa);
 
 
+// // let firstName = "Ashok";
+// // console.log(typeof firstName);
+// // console.log(` My First Name is ${firstName}`);
 
-*/
+// // console.log(firstName)
+
+// // let favoriteFood = "Pizza";
+
+// // console.log(favoriteFood)
+
+// // console.log(`Do you like ${favoriteFood}?`)
+
+// // let email="ashokabrother@gmail.com";
+// // console.log(`Your email is ${email}.`)
+
+
+// let online=false;
+// let forSale=true;
+
+// console.log(typeof online);
+// console.log(`Bro is online: ${online}`)
+// console.log(`This car is for sale: ${forSale}`)
+
+// let isStudent=true;
+
+// console.log(typeof isStudent);
+// console.log(`Is the student enrolled: ${isStudent}`)
+
+let fullName="Ashok Neupane";
+let age=25;
+let isStudent=false;
+
+document.getElementById("p1").textContent=fullName;
+document.getElementById("p2").textContent=age;
+document.getElementById("p3").textContent=isStudent;
+
+
+document.getElementById("p1").textContent=`My full name is ${fullName}.`;
+document.getElementById("p2").textContent=`you are ${age} years old.`;
+document.getElementById("p3").textContent=`Is the student enrolled: ${isStudent}.`;
